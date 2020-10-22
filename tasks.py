6d54a7f331f87a7c5553d70eabb6b8dc62c6ff9e
herukun <herukun100@gmail.com>
2020-10-22 18:34:37 +0300

    update answer

---
+++ tasks.py
@@ -0,0 +1,29 @@
+# Python Variables Tasks:
+## define a variable of type bool 
+isPythonCool = True
+######################################
+## What Is the benefit of the type method
+# To differentiate between each type of variable and value
+#######################################
+## Can we use semi colon ; with python
+# No but in some cases we can 
+############################################
+## Python is interpreted or compiled
+# Python is interpreted language
+###################################################
+## What is the differences between low level & high level 
+# low level = It is non-portable - It is tough to understand 
+# high level = It is portable - It is easy to understand
+#================================================================#
+#Python Operators Tasks :
+## What is the differences between (= , ==)
+#(=) for assignment Values (==) for Comparison of two values
+################################################################
+## What do we mean by using (!=)
+# it means Not equal
+############################################################
+## What is the operator precedence
+##()
+#**
+#/,*
+#+,-
